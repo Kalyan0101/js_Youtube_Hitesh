6ae0e209fa19174a6cfdd8b2f08e5762a07054b9
Kalyan0101 <naskarkalyan2000@outlook.com>
2023-09-20 01:39:51 +0530

    object

--- object.js
+++ object.js
@@ -8,14 +8,25 @@ const obj = {
     age: "23",
     address: "Subhasgram",
     email: "naskarkalyan2@gmail.com",
-    [mySym]: "mykey1",
+    [mySym]: "mykey1"
     // [mySym]: "author"
 };
 
-console.log(obj.address);
-console.log(obj["name"]);
-console.log(obj["full name"]);
-console.log(`${obj.email} \n`);
+// console.log(obj);
+// console.log(obj.address);
+// console.log(obj["name"]);
+// console.log(obj["full name"]);
+// console.log(`${obj.email} \n`);
 
-obj.email = "naskarkalyan2000@gmail.com"
-console.log(obj.email);
+// obj.email = "naskarkalyan2000@gmail.com"
+// console.log(obj.email);
+
+
+obj.greeting = function() {
+    console.log(`Hello Js User, ${this.name}`);
+    return(`Hello Js User`);
+}
+// obj.greeting();
+// console.log(obj.greeting());
+
+console.log(Object.values(obj));
