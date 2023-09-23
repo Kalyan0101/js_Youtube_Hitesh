8a9ec90a6b1c7d83cd344c2644d60087ccbc110a
Kalyan0101 <naskarkalyan2000@outlook.com>
2023-09-23 20:10:04 +0530

    Project 4 Complete

--- project/project4/script.js
+++ project/project4/script.js
@@ -38,20 +38,22 @@
 }
 
 // element selection
-const btn = document.querySelector("#btn")
-const userInp = document.querySelector("#number")
-const preguss = document.querySelector("#last")
-const ramain = document.querySelector("#remain")
-const msg = document.querySelector("#message")
+const btn = document.querySelector("#btn");
+const userInp = document.querySelector("#number");
+const preguss = document.querySelector("#last");
+const remain = document.querySelector("#remain");
+const msg = document.querySelector("#message");
+const startOver = document.querySelector(".resultPara");
 
 // create element
 const p = document.createElement('p');
 
 // variable creation
-let rand = parseInt(Math.random() * 100 +1)
-let preGuess = []
-let guessRem = 1
-let playGame = true
+let rand = parseInt(Math.random() * 100 +1);
+let preGuess = [];
+// let guessRem = 0; **
+let guessRem = (remain.innerHTML);
+let playGame = true;
 
 if(playGame){
     btn.addEventListener("click", (e) =>{
@@ -64,11 +66,19 @@ if(playGame){
 
 //function section
 function inptValidate(value) {
-    if(value === '' || value < 0 && value > 100 || isNaN(value)){
-        showMessage("Please enter a valid number");
+    if(isNaN(value)){
+        showMessage("Please enter a number!");
+
+    }else if(value < 1 || value > 100){
+        showMessage("Please enter a number between 1 to 100!");
+        
     }else{
         preGuess.push(value);
-        if(guessRem === 10){
+
+        // if(guessRem === 10){ **
+        guessRem--;
+
+        if(!guessRem){
             showGuess(value);
             showMessage(`Game Over. Correct number is ${rand}`);
             endGame();
@@ -91,24 +101,42 @@ function checkGuess(value){
 }
 
 function showGuess(guess) {
-    userInp.value = ""
-    preguss.innerHTML += `${guess}  `
-    guessRem++;
-    ramain.innerHTML = `${11 - guessRem}`
+    userInp.value = "";
+    preguss.innerHTML += `${guess}  `;
+
+    // guessRem++; ** position changed^
+    // remain.innerHTML = `${10 - guessRem}`; **
+
+    remain.innerHTML = `${guessRem}`;
 }
 
 function showMessage(message) {
-
     // p.appendChild(document.createTextNode(message));
     // msg.appendChild(p)
-
     msg.innerHTML = `<h2> ${message} </h2>`;
 }
 
 function endGame(){
-    
+    userInp.value = '';
+    userInp.setAttribute('disabled', '');
+    p.classList.add('button');
+    p.innerHTML = `<h2 id="newGame">Start New Game</h2>`;
+    startOver.appendChild(p);
+    playGame = false;
+    newGame();
 }
 
 function newGame(){
-    //
+    const newGameButton = document.querySelector("#newGame");
+    newGameButton.addEventListener("click", (e) =>{
+        
+        rand = parseInt(Math.random() * 100 +1);
+        preGuess = [];
+        guessRem = 10
+        preguss.innerHTML = '';
+        userInp.removeAttribute('disabled', '');
+        startOver.removeChild(p);
+        showMessage('');
+        playGame = true;
+    })
 }
